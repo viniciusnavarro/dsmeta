82de32830743ef4c53818f48c3edb5142f5bc353
Vinicius Navarro <vinicius.navarro@matera.com>
2022-11-14 19:02:32 -0300

    Clean project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,3 +8,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>
 )
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
